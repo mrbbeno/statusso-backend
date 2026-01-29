704f1a29e4adf7707bf045ad159acc3cf05621f0
mrbbeno <balazs.benedek2006@gmail.com>
2026-01-29 23:04:48 +0100

    Frissítések feltöltése

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM Első parancssor megnyitása és parancs futtatása az első mappában
-start cmd /k "cd /d D:\satusso\backend && node server.js"
-
-REM Második parancssor megnyitása és parancs futtatása a második mappában
-start cmd /k "cd /d D:\satusso\backend\statusso-frontend && npm start"
